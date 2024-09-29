2263a3f059a5b8da397d73275d0cbd9f806c0185
Hakho05 <daubykyllian04@gmail.com>
2024-09-29 12:48:47 +0200

    vue liste données contenue

---
+++ vue/vueli-données.php
@@ -0,0 +1,12 @@
+<body>
+<h1>Liste des données nécessaires</h1>
+<ul>
+    <li>Nom</li>
+    <li>Prénom</li>
+    <li>E-mail</li>
+    <li>Numéro de telephone</li>
+</ul>
+    Ces informations nous sont utiles dans le cadre des contacts avec vous. 
+    En effets, nous avons besoins de ses coordonnées pour pouvoir répondre 
+    à vos demandes de contactes ou pour tout autre raisons (avertissement...).
+</body>
