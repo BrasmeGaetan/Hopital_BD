548dde9821b8fa2d6a29277b627cfdc2dcf50a0e
gaetan <brasmegaetan0@gmail.com>
2024-09-29 17:34:34 +0200

    Creation inscription ainsi que creation d'un footer

---
+++ vue/deconnexion.php
@@ -0,0 +1,12 @@
+<?php
+session_start(); // Démarrer la session
+
+// Détruire la session
+unset($_SESSION['valid']);
+session_unset();
+session_destroy();
+
+// Rediriger vers la page de connexion
+header("Location: connexion.php");
+exit();
+?>
