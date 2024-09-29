39c7ad62a2ad8cf0042a0e586152760e89953e6c
Hakho05 <146886482+Hakho05@users.noreply.github.com146886482+Hakho05@users.noreply.github.com>
2024-09-29 16:33:52 +0200

    commit liste

--- vue/vueli-données.php
+++ vue/vueli-données.php
@@ -5,8 +5,9 @@
     <li>Prénom</li>
     <li>E-mail</li>
     <li>Numéro de telephone</li>
+    <li>Votre rang sur Valorant (données inutile pour l'exemple)</li>
 </ul>
     Ces informations nous sont utiles dans le cadre des contacts avec vous. 
     En effets, nous avons besoins de ses coordonnées pour pouvoir répondre 
-    à vos demandes de contactes ou pour tout autre raisons (avertissement...).
+    à vos demandes de contactes ou pour tout autre raisons (avertissement, etc...).
 </body>
