097f2b879e2f88c8ad480900ffee8ce58df0fd47
gaetan <brasmegaetan0@gmail.com>
2024-09-29 17:45:05 +0200

    ajout deconnexion

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <?php
-    include "inc/header.inc";
-    session_start();
-    include "controleur/controleurPrincipal.php";
+session_start(); // Assurez-vous que la session est démarrée au tout début
 
-    if (isset($_GET["action"])) {
-        $action = $_GET["action"];
-    } else {
-        $action = "index";
-    }
+// Vérification de la déconnexion
+if (isset($_GET["action"]) && $_GET["action"] == "deconnexion") {
+    // Gestion de la déconnexion
+    unset($_SESSION['valid']);
+    session_unset();
+    session_destroy();
+    header("Location: ./index.php?action=index"); // Redirige vers la page d'accueil ou l'index
+    exit(); // Sortir du script pour éviter d'autres actions
+}
 
-    $fichier = controleurPrincipal($action);
+include "inc/header.inc"; // Inclusion de l'en-tête
+include "controleur/controleurPrincipal.php"; // Inclusion du contrôleur principal
 
-    include "controleur/$fichier";
-    include "inc/footer.inc";
+// Vérification de l'action
+if (isset($_GET["action"])) {
+    $action = $_GET["action"];
+} else {
+    $action = "index"; // Action par défaut
+}
+
+$fichier = controleurPrincipal($action); // Obtenir le fichier à inclure
+include "controleur/$fichier"; // Inclusion du fichier de contrôleur
+include "inc/footer.inc"; // Inclusion du pied de page
