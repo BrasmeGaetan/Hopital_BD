1778b72fa5aa242a72753fc3f3e9ab9f7f46a6d1
Hakho05 <daubykyllian04@gmail.com>
2024-09-30 08:31:41 +0200

    Mise dans le header de la page cookies

--- autre/configDB.php
+++ autre/configDB.php
@@ -1,5 +1,5 @@
 <?php
 
 $bdd = 'mysql:host=localhost;dbname=dblogin5265';
-$user = 'login5265';
-$password = 'FEqfhWBHpwXSCzn';
+$user = 'login4544';
+$password = 'ydtkfzeMbBxlKik';
--- controleur/li-donnees.php
+++
@@ -1,4 +0,0 @@
-<?php
-include "vue/vueli-donnees.php";
-
-?>
